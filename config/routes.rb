1ed33ec082ead5661115e36351afec28ddaed670
Cristina Souza <cristina.carvalho.b@gmail.com>
2019-07-15 21:46:42 -0700

    Rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :home, only: [:index]
-  resources :about, only: [:index]
+  resources :homes, only: [:index]
+  resources :abouts, only: [:index]
 
 end
