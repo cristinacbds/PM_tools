4c81a054aafabe900832ea38acb4298811dc01e3
Cristina Souza <cristina.carvalho.b@gmail.com>
2019-07-16 21:37:23 -0700

    add the edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,16 +6,33 @@ class ProjectsController < ApplicationController
     def create
         @project = Project.new(project_params)
         @project.save
-        p @project.errors
         if @project.save
-            p @project
-            redirect_to projects_path(@project)
+            redirect_to project_path(@project)
         else
             render 'new'
         end
     end
 
+    def show
+        @project = Project.find(params[:id])
+    end
 
+    def update
+        @project = Project.find(params[:id])
+        if @project.update(project_params)
+            redirect_to project_path(@project)
+        else
+            render 'edit'
+        end
+    end
+
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def index
+        @projects = Project.all
+    end
 
     private
 
